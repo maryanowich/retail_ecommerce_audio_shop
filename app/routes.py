367ba85c2383b35e582444913ffaf39197bb7c94
maryanowich <105643117+maryanowich@users.noreply.github.com>
2026-04-08 07:57:14 +0200

    fix routes  admin rute

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,7 @@
-from flask import Blueprint, render_template
-from app.models import Product
+from flask import Blueprint, redirect
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    products = Product.query.all()
-    return render_template("index.html", products=products)
+    return redirect("/admin")
